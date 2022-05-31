ef7870c8ecae17761f5865314da6c2ede15fcb16
Avielteka <aviek2260@gmail.com>
2022-05-31 21:23:50 +0300

    add

---
+++ main.js
@@ -0,0 +1,80 @@
+// dom 1
+// console.log(first.innerText);
+
+// dom 2
+// var nam = getElementsById
+// ("blan");
+// document.getElementById("blan").innerText = "my dom app";
+// dom 3
+// var class1 = document.getElementsByClassName("avi");
+// console.log(class1);
+
+// dom 4
+// console.log(class1[2].innerText);
+
+// dom 5
+// var elem2 = document.getElementsByClassName("avi");
+// console.log(elem2[1].innerHTML);
+
+// dom 6
+// var elem = document.getElementsByClassName("h_1");
+// for (var i =0; i < elem.length; i++){
+//     console.log(elem[i].innerText)
+// }
+
+// dom 7
+//  function printH2(){
+//     var username = prompt("enter name")
+//      document.write("<h2>" + username + "</h2>");
+//  }
+//  printH2();
+
+// dom 8
+// function printColor(){
+//     var color = prompt("enter a color")
+//     var num = +prompt("enter a number")
+//     for(var i =0; i < num; i ++) 
+//     document.write('<span>'+color+ '</span>'+'<br>')
+// }
+// printColor()
+
+// dom 9
+// function creatTag(){
+//     var text = prompt("enter text")
+//     var tags = prompt("enter a tag")
+//     document.write(tags + text)
+// }
+// creatTag()
+
+
+//  dom 10
+//  function getInput() {
+//      var inpuType = prompt("enter input type")
+//     //  document.write('<input type ='  + inpuType + '</input>')
+//     document.write( '<input type= "' + inpuType +  '"></input>')
+//  }
+//  getInput()
+
+// dom 11
+// function changeContact(className,text1){
+//     for(var i = 0; i<3; i++){
+//         document.getElementsByClassName(className)[i].innerText = text1 
+//     }
+// }
+// changeContact("avi",prompt("text1"))
+
+// dom 12
+// function enterElements(){
+//     for(var i = 0; i<6; i++){
+//     document.getElementsByTagName("p")[i].innerHTML = "fuck yall"
+//     }
+// } 
+// enterElements()
+
+// dom 13
+
+// dom 17
+// function printShops(){
+//     var numuser = prompt("enter number");
+
+// } 
